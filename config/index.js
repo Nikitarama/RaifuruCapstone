6bd0b9b3cf5c6940515095b170236bcba5c8a947
Nikitarama <https://github.com/Nikitarama/MyPortfolio.git>
2023-03-14 16:48:09 +0200

    5th

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 require ('dotenv').config();
-const { createPool } = require('mysql2');
+const { createPool } = require('mysql');
 let connection = createPool ({
     host: process.env.dbHost,
     user: process.env.dbUser,
